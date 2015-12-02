579aa61dc867ac31882eea5137f586a7ab9df4b6
Stef Broenink <sea4nl@gmail.com>
2015-12-02 21:08:16 +0100

    js stuff

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,5 @@
+<script>
+Vue.component('Home', {
+	template: '<h1>Home hier</h1>'
+});
+</script>
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,5 +1,43 @@
 var Vue = require('vue');
+var VueRouter = require('vue-router')
+Vue.use(VueRouter)
 
-new Vue({
-	el: '#app'
+// Define some components
+
+import Home from './components/Home.vue';
+
+
+var Dashboard = Vue.extend({
+    template: '<p>This is Dashboard!</p>'
 })
+
+var Page = Vue.extend({
+    template: '<p>This is Page!</p>'
+})
+
+var User = Vue.extend({
+    template: '<p>This is User!</p>'
+})
+
+
+var App = Vue.extend({
+})
+
+var router = new VueRouter()
+
+router.map({
+    '/home': {
+        component: Home
+    },
+    '/dashboard': {
+        component: Dashboard
+    },
+    '/page': {
+        component: Page
+    },
+    '/user': {
+        component: User
+    }
+})
+
+router.start(App, '#app')
